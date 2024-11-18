b3773c98e4c7cfcda164dc95d8414048d121cf99
Agrama26 <ramadhanagung1112@gmail.com>
2024-11-18 11:03:12 +0700

    css

--- input.php
+++ input.php
@@ -90,8 +90,8 @@ $conn->close();
 
             <!-- Tombol ke Dashboard -->
             <div style="margin-top: 10px;">
-                <button type="submit" class="col-6 mt-3 mb-3 btn btn-primary">Add User</button>
-                <a href="dashboard.php" class="col-6 dashboard-btn">List User</a>
+                <button type="submit" class="col-2 btn btn-primary">Add</button>
+                <a href="dashboard.php" class="col-2 btn-dashboard">List</a>
             </div>
         </form>
     </div>
@@ -103,17 +103,17 @@ $conn->close();
     </div>
 
     <script>
-    // Animasi saat halaman dibuka
-    window.onload = function() {
-        document.getElementById("login-card").classList.add("show");
-    }
+        // Animasi saat halaman dibuka
+        window.onload = function () {
+            document.getElementById("login-card").classList.add("show");
+        }
     </script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
         integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
-    </script>
+        </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
         integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous">
-    </script>
+        </script>
 </body>
 
 </html>
